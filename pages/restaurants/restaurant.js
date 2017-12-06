b29fcaf44a928cba281a97c57f67af3ac5c0fbef
Kabirova Alina <kabirova.alina@icloud.com>
2017-12-06 15:01:45 +0800

    Location tap and fixed user uuid

--- pages/restaurants/restaurant.js
+++ pages/restaurants/restaurant.js
@@ -4,25 +4,25 @@ Page({
   }
 })
 
-// pages/restaurants/restaurant.js
-// Page({
+// // pages/restaurants/restaurant.js
+// // Page({
 
-  /**
-   * 页面的初始数据
-   */
-//   data: {
-//     imgUrls: [
-//       '#'
-//       '#'
-//       '#'
-//     ]
-//     indicatorDots: true,
-//     indicator-color: rgba(245, 166, 35, 100;
-//     indicator-active - color: rgba(235, 122, 39, 40); 
-//     autoplay: false,
-//     interval: 5000,
-//     duration: 1000,
-//   },
+//   /**
+//    * 页面的初始数据
+//    */
+// //   data: {
+// //     imgUrls: [
+// //       '#'
+// //       '#'
+// //       '#'
+// //     ]
+// //     indicatorDots: true,
+// //     indicator-color: rgba(245, 166, 35, 100;
+// //     indicator-active - color: rgba(235, 122, 39, 40); 
+// //     autoplay: false,
+// //     interval: 5000,
+// //     duration: 1000,
+// //   },
 
 //   /**
 //    * 生命周期函数--监听页面加载
