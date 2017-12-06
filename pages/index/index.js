aa0ed5256b91ccd99e7b13009ea70bff2dae16ae
Kabirova Alina <kabirova.alina@icloud.com>
2017-12-06 13:58:43 +0800

    added location

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,35 @@
 //index.js
 //获取应用实例
-const app = getApp()
-
+var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  onLoad: function(options) {
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+  listenerBtnGetLocation: function () {
+    wx.getLocation({
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        var speed = res.speed
+        var accuracy = res.accuracy
+        console.log(res)
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   }
+  // bindLocationTap: function() {
+  //   console.log('It finally works');
+  //   wx.getLocation({
+  //     success: function(res) {
+  //       var latitude = res.latitude
+  //       var longitude = res.longitude
+  //       console.log(longitude)
+  //       console.log(latitude);
+  //     },
+  //   })
+  // },
 })
