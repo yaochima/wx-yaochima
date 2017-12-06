0593dae3a87dd4b4dce94607402ec99b7982905d
Kabirova Alina <kabirova.alina@icloud.com>
2017-12-06 13:58:43 +0800

    added location

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ App({
     uuid: null,
     lat: null,
     lng: null
-  },
-  onLaunch: function () {
-    const that = this
-    // 展示本地存储能力
-    const userUuid = wx.getStorageSync('uuid') || ""
+  }
+onLaunch: function () {
+const that = this
+// 展示本地存储能力
+  const userUuid = wx.getStorageSync('uuid') || ""
     if (userUuid === "") {
       that.globalData.uuid = that.createUUID()
       wx.setStorageSync('uuid', that.globalData.uuid)
     } else {
       that.globalData.uuid = userUuid
-    };
-  },
+    },
     createUUID: function() {
       let s = [];
       const hexDigits = "0123456789abcdef";
@@ -29,43 +28,39 @@ App({
       const uuid = s.join("");
       return uuid;
     }
-  })  
-  //  // wx.checkSession interface test whether the current user login status is valid.
-  //   wx.checkSession({
-  //     success: function () {    //登录态未过期
-  //     },
-  //     fail: function () {    //登录态过期
-  //       wx.login()
-  //     }
-  //   })
+   }   // wx.checkSession interface test whether the current user login status is valid.
+    // wx.checkSession({
+    //   success: function () {    //登录态未过期
+    //   },
+    //   fail: function () {    //登录态过期
+    //     wx.login()
+    //   }
+    // })
 
+    // 登录 Log in
+    // wx.login({
+    //   success: res => {
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //   }
+    // })
+    // // 获取用户信息 Get User Details 
+    // wx.getSetting({
+    //   success: res => {
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //       wx.getUserInfo({
+    //         success: res => {
+    //           // 可以将 res 发送给后台解码出 unionId
+    //           this.globalData.userInfo = res.userInfo
 
-//     // 登录 Log in
-//     wx.login({
-//       success: res => {
-//         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-//       }
-//     })
-//     // 获取用户信息 Get User Details 
-//     wx.getSetting({
-//       success: res => {
-//         if (res.authSetting['scope.userInfo']) {
-//           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-//           wx.getUserInfo({
-//             success: res => {
-//               // 可以将 res 发送给后台解码出 unionId
-//               this.globalData.userInfo = res.userInfo
-
-//               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-//               // 所以此处加入 callback 以防止这种情况
-//               if (this.userInfoReadyCallback) {
-//                 this.userInfoReadyCallback(res)
-//               }
-//             }
-//           })
-//         }
-//       }
-//     })
-//   }
-  
-// })
+    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //           // 所以此处加入 callback 以防止这种情况
+    //           if (this.userInfoReadyCallback) {
+    //             this.userInfoReadyCallback(res)
+    //           }
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
+  })
